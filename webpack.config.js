c7f04797616c267af919dbf473d3d3265f9cb03a
Phil Chan <phillipchan1@gmail.com>
2019-09-08 16:11:32 -0700

    Add webpack, tsconfig, ts, prettier, package.json scripts

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+const webpack = require('webpack')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.ts'),
+  output: {
+    filename: 'dist/index.js',
+    path: __dirname,
+    // devtoolNamespace: 'Parallax',
+    libraryTarget: 'var',
+    // `library` determines the name of the global variable
+    library: 'MegaMenu'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  watch: true
+}
