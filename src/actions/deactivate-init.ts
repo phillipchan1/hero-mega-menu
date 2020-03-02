ebbbd7ae3134b8db90b7cda302ea67c8c0c5166d
Phil <phillipchan1@gmail.com>
2020-03-01 17:23:56 -0800

    remove comment

--- src/actions/deactivate-init.ts
+++ src/actions/deactivate-init.ts
@@ -4,9 +4,4 @@ import { deactivateMenu } from './deactivate-menu'
 export const deactivateInit = (config, menuElements) => {
   deactivateMenu(config.menuDropClass)
   toggleVisibilityOfNormalMenu(config.overrideMenuClass, true)
-
-  // menuElements.forEach(menu => {
-  //   menu.removeEventListener('mouseenter')
-  //   menu.removeEventListener('moouseleave')
-  // })
 }
